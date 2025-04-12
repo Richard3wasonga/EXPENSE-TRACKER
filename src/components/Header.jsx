85351545958ab74f457bd41e7ffd2288f9d9bc14
richard <richard.wasonga@student.moringaschool.com>
2025-04-12 13:11:57 +0300

    corrected page title typo and site title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='headerdis'>
-        <h1>Expense Traker</h1>
+        <h1>Expense Tracker</h1>
         <p>Start taking control of your finances and life.Record<br/>
             categorize and analyze your spending
         </p>
